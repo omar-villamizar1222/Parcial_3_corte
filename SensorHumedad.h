bf3f71e3cef7c50fa52065629e2241e921d5a203
omar-villamizar1222 <ovillamizar022@gmail.com>
2026-06-13 15:02:41 -0500

    se agrega el nombre de lo que se esta midiendo

--- SensorHumedad.h
+++ SensorHumedad.h
@@ -7,25 +7,28 @@
  * @brief Clase que representa un sensor de humedad, hereda de la clase Sensor y implementa los métodos para leer e imprimir el valor del sensor.
  */
 class SensorHumedad : public Sensor {
+    
     private:
         int pin; // Pin al que está conectado el sensor de humedad
-        int valor; // Valor leído del sensor de humedad
+        float valor; // Valor leído del sensor de humedad
+        std::string nombre; // Nombre del sensor 
     
     public:
         /* @brief Constructor de la clase SensorHumedad, inicializa el pin y el nombre del sensor */
         // @param Pin al que está conectado el sensor de humedad
         // @param Nombre del sensor de humedad
         // @param porcentaje de humedad leído por el sensor
-        SensorHumedad(int pin, int valor);
+        SensorHumedad(int pin, float valor, std::string nombre);
         /* @brief Método para leer el valor del sensor de humedad, devuelve un porcentaje de humedad */
         // @return Porcentaje de humedad leído por el sensor
-        int leerValor() override;
+        float leerValor() override;
         /* @brief Método para imprimir el valor del sensor de humedad en la LCD 16x2 */ 
         void imprimirValor() override;
         
         //getters 
         int getPin() const; // Método para obtener el pin del sensor de humedad
-        int getValor() const; // Método para obtener el valor del sensor de humedad
+        float getValor() const; // Método para obtener el valor del sensor de humedad
+        std::string getNombre() const; // Método para obtener el nombre del sensor de humedad
 };
 
 #endif
--- Sensores.h
+++ Sensores.h
@@ -1,14 +1,13 @@
 #ifndef SENSORES
 #define SENSORES    
 
-#include <String.h>
+#include <string>
 /*@brief clase base abstracta para los sensores, con un metodo virtual puro para obtener la lectura del sensor*/
 class Sensor {
-    public:
-        virtual float leer() = 0; // metodo virtual puro para obtener la lectura del sensor
+
     protected:
         int pin; // pin al que esta conectado el sensor
-        int valor; // valor leido del sensor
+        float valor; // valor leido del sensor
         std::string nombre; // nombre del sensor
 
 /* @brief Constructor de la clase Sensor. 
@@ -18,7 +17,8 @@ class Sensor {
  */
 
  public:
-    Sensor(int valor, int pin, std::string nombre);
+    
+    Sensor(float valor, int pin, std::string nombre);
 
     virtual ~Sensor() = default; // destructor virtual por defecto
 
@@ -26,8 +26,8 @@ class Sensor {
 
     virtual void imprimirValor() = 0; // metodo virtual puro para imprimir el valor del sensor
 
-    std::string getNombre() const; // metodo para obtener el nombre del sensor
-    int getValor() const; // metodo para obtener el valor del sensor
+    float getValor() const; // metodo para obtener el valor del sensor
     int getPin() const; // metodo para obtener el pin del sensor
+    std::string getNombre() const; // metodo para obtener el nombre del sensor
 };
 #endif
